c3bc9d9ae17de063a3cc01de8554d6fd38437a62
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-23 18:08:04 +0530

    pushing mychanges

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
+import Carasoul from './components/CarasoulPart/Carasoul';
+
 import Topbar from './components/Topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar/>
+      <Carasoul/>
     </div>
   );
 }
