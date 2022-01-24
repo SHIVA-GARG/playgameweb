9c5a4ca7825072fb96f4db9086fe2b5e59f8bda2
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-24 09:26:25 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Carasoul from './components/CarasoulPart/Carasoul';
+// import Carasoul from './components/CarasoulPart/Carasoul';
+import Carasoul from './components/CarasoulPart/Carasoul'
 import Footer from './components/footer/Footer';
 import Middlepart from './components/middlePart/Middlepart';
 import './App.css'
