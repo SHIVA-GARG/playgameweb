b8dada2e4d2841719431cb5ad94f574470413924
Suryansh <sgoyal2402@gmail.com>
2022-01-24 22:54:34 +0530

    minor changes

--- src/components/CarasoulPart/productCart/ProductCart.jsx
+++ src/components/CarasoulPart/productCart/ProductCart.jsx
@@ -6,7 +6,7 @@ function ProductCart({ h1, span, p }) {
     <h6>{h1} <span>{span}</span></h6>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt deleniti tempora dignissimos nostrum nihil molestiae sunt placeat dolorum eveniet ducimus veniam, tenetur eos quasi aliquid consequatur aliquam at ipsam consectetur!
       Itaque ducimus</p>
-    <a href='#'>load more
+    <a href='#1'>load more
       <ChevronRight className="icon" htmlColor='white' />
     </a>
   </div>;  </div>)
