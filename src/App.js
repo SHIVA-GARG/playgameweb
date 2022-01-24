d1fe4be5ac4d31e016664cd095c5ef6df96a421b
Shiva Garg <66199185+SHIVA-GARG@users.noreply.github.com>
2022-01-24 09:22:23 +0530

    progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Carasoul from './components/CarasoulPart/Carasoul';
 import Footer from './components/footer/Footer';
 import Middlepart from './components/middlePart/Middlepart';
-
+import './App.css'
 import Topbar from './components/Topbar/Topbar';
 
 function App() {
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -24,42 +24,42 @@ const FooterDiv = styled.div`
 const Footer = () => {
     return (
         <FooterDiv>
-            <div className="container-fluid" style={{width: "80%"}}>
-           <div className="row">
-               <div className="col-lg-4">
-                   <h4> <span> LAUNCHPAD & </span> IGO</h4>
-                   <p></p>
-               </div>
-               <div className="col-lg-5 row">
-                   <div className="col-lg-4">
-                    <h4> ABOUT </h4>
-                    <ul>
-                        <li>main website</li>
-                        <li>privacy policy</li>
-                        <li>terms of services</li>
-                        <li>legal overview</li>
-                    </ul>
-                   </div>
-                   <div className="col-lg-4">
-                    <h4> LINKS </h4>
-                    <ul>
-                        <li>main website</li>
-                        <li>privacy policy</li>
-                        <li>terms of services</li>
-                        <li>legal overview</li>
-                    </ul>
-                   </div>
-                   <div className="col-lg-4">
-                       <h4> CONTACT </h4>
-                       <p>info@oasismetaverse.studio</p>
-                       <p>info@knightwar.io</p>
-                   </div>
+            <div className="container-fluid" style={{ width: "80%",color:"white" }}>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <h4> <span> LAUNCHPAD & </span> IGO</h4>
+                        <p></p>
+                    </div>
+                    <div className="col-lg-5 row">
+                        <div className="col-lg-4">
+                            <h4> ABOUT </h4>
+                            <ul>
+                                <li>main website</li>
+                                <li>privacy policy</li>
+                                <li>terms of services</li>
+                                <li>legal overview</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-4">
+                            <h4> LINKS </h4>
+                            <ul>
+                                <li>main website</li>
+                                <li>privacy policy</li>
+                                <li>terms of services</li>
+                                <li>legal overview</li>
+                            </ul>
+                        </div>
+                        <div className="col-lg-4">
+                            <h4> CONTACT </h4>
+                            <p>info@oasismetaverse.studio</p>
+                            <p>info@knightwar.io</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-3">
+                        <h4> JOIN OUR COMMUNITY </h4>
+                    </div>
                 </div>
-               <div className="col-lg-3">
-                     <h4> JOIN OUR COMMUNITY </h4>
-               </div>
-           </div>
-           </div>
+            </div>
         </FooterDiv>
 
     )
